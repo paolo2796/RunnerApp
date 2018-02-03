a137b30640fe7469060179f3e27d0520bd591d6b
paolo2796 <paolovigorito96@gmail.com>
2018-02-03 10:54:43 +0100

    Fixed Bug annuncio

---
+++ app/src/main/java/it/unisa/runnerapp/fragments/AdsActiveFragment.java
@@ -0,0 +1,44 @@
+package it.unisa.runnerapp.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+import it.unisa.runnerapp.Dao.Implementation.ActiveRunDaoImpl;
+import it.unisa.runnerapp.Dao.Interf.ActiveRunDao;
+import it.unisa.runnerapp.adapters.AdActiveAdapter;
+import it.unisa.runnerapp.beans.ActiveRun;
+import testapp.com.runnerapp.R;
+
+/**
+ * Created by Paolo on 03/02/2018.
+ */
+
+
+public class AdsActiveFragment extends Fragment {
+
+     List<ActiveRun> runsactive;
+     ListView listview;
+     public AdActiveAdapter arrayadapter;
+     ActiveRunDao activerundao;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.adsactive_fragment, container, false);
+
+        listview = (ListView) v.findViewById(R.id.listview);
+        List<ActiveRun> runs = new ActiveRunDaoImpl().getActiveRunsWithin24h("data_inizio");
+        arrayadapter = new AdActiveAdapter(this.getActivity(),R.layout.row_adactive,runs);
+        listview.setAdapter(arrayadapter);
+        return v;
+    }
+
+
+
+}
