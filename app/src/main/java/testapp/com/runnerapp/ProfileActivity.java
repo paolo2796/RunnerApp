0cfc3421626bc48c43adbaf7736df0099cb9ba54
paolo2796 <paolovigorito96@gmail.com>
2018-02-19 15:45:25 +0100

    - Implementazione Visualizzazione Profilo Utente

--- app/src/main/java/testapp/com/runnerapp/ProfileActivity.java
+++ app/src/main/java/testapp/com/runnerapp/ProfileActivity.java
@@ -6,24 +6,41 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+
+    //Component View
+    TextView nametw;
+    TextView surnametw;
+    TextView nicknametw;
+    TextView leveltw;
+    TextView travelledkmtw;
+    ImageView imageprofile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        nametw = findViewById(R.id.name_tw);
+        surnametw = findViewById(R.id.surname_tw);
+        nicknametw = findViewById(R.id.nickname_tw);
+        leveltw = findViewById(R.id.level_tw);
+        travelledkmtw = findViewById(R.id.travelledkm_tw);
+        imageprofile = findViewById(R.id.masterprofile_img);
+
+
+        imageprofile.setImageDrawable(MainActivityPV.userlogged.getProfileImage());
+        nametw.setText(MainActivityPV.userlogged.getName());
+        surnametw.setText(MainActivityPV.userlogged.getSurname());
+        nicknametw.setText(MainActivityPV.userlogged.getNickname());
+        leveltw.setText(String.valueOf(MainActivityPV.userlogged.getLevel()));
+        travelledkmtw.setText(String.valueOf(MainActivityPV.userlogged.getTraveledKilometers()));
+
     }
 
 }
